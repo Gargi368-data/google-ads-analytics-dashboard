14c8f607ee9f92bad931e8371f7cafd1949fafc4
Gargi368-data <gargi.ai123@gmail.com>
2026-01-16 00:37:34 +0530

    Update and rename .gitkeep to create_tables.sql

---
+++ sql/sql/create_tables.sql
@@ -0,0 +1,12 @@
+
+create table google_ads_weekly (
+  week_start date,
+  campaign text,
+  ad_group text,
+  impressions int8,
+  clicks int8,
+  cost int8,
+  conversions int8,
+  device text,
+  locatiob text,
+  channel text )
