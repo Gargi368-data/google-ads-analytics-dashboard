a56d92cf0758a7de18b7b3415eea842855b54bbc
Gargi368-data <gargi.ai123@gmail.com>
2026-01-16 00:23:41 +0530

    Create create_tables.sql

---
+++ data/sql/.gitkeep powerbi/.gitkeep screenshots/create_tables.sql
@@ -0,0 +1,11 @@
+Create table google_ads_weekly (
+  week_start date,
+  campaign text,
+  ad_group text,
+  impressions int8,
+  clicks int8,
+  cost numeric,
+  conversions int8,
+  device text,
+  location text,
+  channel text )
